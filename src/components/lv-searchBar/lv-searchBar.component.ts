1bf8aeddaa24f12b90bf8f02d3af9a82a4f3b8af
Levi Jefte Vicente Navarro <hiberus_ljvicente@sacyr.com>
2023-03-06 17:36:59 +0100

    continuar search

--- src/components/lv-searchBar/lv-searchBar.component.ts
+++ src/components/lv-searchBar/lv-searchBar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Component, Input, OnInit, ViewContainerRef } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { ModalType } from 'src/constants/lv-constans';
+import { LvModalService } from 'src/services/lv-modal.service';
 
 @Component({
-  selector: 'app-lv-searchBar',
-  templateUrl: './lv-searchBar.component.html',
-  styleUrls: ['./lv-searchBar.component.css']
+    selector: 'app-lv-searchBar',
+    templateUrl: './lv-searchBar.component.html',
+    styleUrls: ['./lv-searchBar.component.scss']
 })
 export class LvSearchBarComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    @Input() url!: string;
+    results: any[] = [];
+
+    constructor(private http: HttpClient, private modalService: LvModalService, private viewRef: ViewContainerRef) { }
+
+    ngOnInit() {
+    }
+
+    async search(userInput: string) {
+        const params = new HttpParams().set('filter', userInput);
+        this.http.get(this.url).subscribe((data: any) => {
+            this.results = data;
+            console.log(this.results);
+        });
+    }  
 
 }
